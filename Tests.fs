ea9ecdadd29e03cdb5eb5513c4c969cfaf156846
Tonino Lucca <tonyx1@gmail.com>
2026-07-08 14:52:42 +0200

    first commit

---
+++ Tests.fs
@@ -0,0 +1,75 @@
+module Tests
+open DotNetEnv
+
+open Expecto
+open System
+open Sharpino
+open Sharpino.EventBroker
+open Sharpino.Template.Models
+open Sharpino.Cache
+open Sharpino.CommandHandler
+open Sharpino.Template
+open Sharpino.Template.Commons
+open FsToolkit.ErrorHandling
+open Sharpino
+open Sharpino.MemoryStorage
+
+
+let memEventStore = MemoryStorage()
+
+let setUp () =
+    memEventStore.Reset Todo.Version Todo.StorageName |> ignore
+    AggregateCache3.Instance.Clear()
+    DetailsCache.Instance.Clear()
+
+let memTodoViewer = getAggregateStorageFreshStateViewer<Todo, TodoEvents, string> memEventStore
+
+[<Tests>]
+let tests =
+    testList "todos tests" [
+        testCase "add and retrieve a todo" <| fun _ ->
+            setUp ()
+            let todoManager = TodoManager (MessageSenders.NoSender, memEventStore, memTodoViewer)
+            let learnFSharp = Todo.New "Learn F#"
+            let addLearnFSharp = todoManager.AddTodo learnFSharp
+            Expect.isOk addLearnFSharp "error in adding todo"
+            let retrievedTodo = todoManager.GetTodo learnFSharp.TodoId
+            Expect.isOk retrievedTodo "error in retrieving todo"
+
+        testCase "add two todos and retrieve all todos" <| fun _ ->
+            setUp ()
+            let todoManager = TodoManager (MessageSenders.NoSender, memEventStore, memTodoViewer)
+            let learnFSharp = Todo.New "Learn F#"
+            let learnRust = Todo.New "Learn Rust"
+            let addLearnFSharp = todoManager.AddTodo learnFSharp
+            let addLearnRust = todoManager.AddTodo learnRust
+            Expect.isOk addLearnFSharp "error in adding todo"
+            Expect.isOk addLearnRust "error in adding todo"
+            let retrievedTodos = 
+                todoManager.GetTodosAsync ()
+                |> Async.AwaitTask
+                |> Async.RunSynchronously
+                |> Result.get
+            Expect.hasLength retrievedTodos 2 "error in retrieving todos"
+            Expect.isTrue (retrievedTodos |> List.exists (fun x -> x = learnFSharp)) "error in retrieving todos"
+            Expect.isTrue (retrievedTodos |> List.exists (fun x -> x = learnRust)) "error in retrieving todos"
+
+        testCaseTask "add two todos and retrieve all todos using task" <| fun _ ->
+            task {
+                setUp ()
+                let todoManager = TodoManager (MessageSenders.NoSender, memEventStore, memTodoViewer)
+                let learnFSharp = Todo.New "Learn F#"
+                let learnRust = Todo.New "Learn Rust"
+                let addLearnFSharp = todoManager.AddTodo learnFSharp
+                let addLearnRust = todoManager.AddTodo learnRust
+                Expect.isOk addLearnFSharp "error in adding todo"
+                Expect.isOk addLearnRust "error in adding todo"
+                let! retrievedTodos = 
+                    todoManager.GetTodosAsync () 
+                let retrievedTodos =retrievedTodos |> Result.get
+                Expect.hasLength retrievedTodos 2 "error in retrieving todos"
+                Expect.isTrue (retrievedTodos |> List.exists (fun x -> x = learnFSharp)) "error in retrieving todos"
+                Expect.isTrue (retrievedTodos |> List.exists (fun x -> x = learnRust)) "error in retrieving todos"
+            }
+    ] 
+    |> testSequenced
